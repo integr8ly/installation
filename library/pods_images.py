e444fd2caa126e4729502357e5454778a03423b3
Leonardo Rossetti <lrossett@redhat.com>
2019-05-09 09:38:15 +0100

    image sync

--- library/pods_images.py
+++ library/pods_images.py
@@ -2,31 +2,36 @@ from ansible.module_utils.basic import AnsibleModule
 
 import json
 
-try:
-    import jsonpointer
-except ImportError:
-    jsonpointer = None
-
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            pods=dict(required=True, type='list'),
+            data=dict(required=True, type='list'),
+            skip_os3=dict(type='bool', default=True),
+            skip_internal=dict(type='bool', default=True)
         ),
         supports_check_mode=True,
     )
 
+    skip_os3 = module.params['skip_os3']
+    skip_internal = module.params['skip_internal']
     pods = module.params['data']
     if isinstance(pods, str):
-        pods = json.loads(str)
+        pods = json.loads(pods)
     images = []
 
     for pod in pods:
         containers = pod['spec']['containers']
         for container in containers:
-            images.append(container['image'])
+            if 'docker-registry.default.svc' in container['image'] or '/openshift3/' in container['image']:
+                continue
+            parts = container['image'].split(':')
+            images.append({
+                'image': parts[0].replace('@sha256', ''),
+                'tag': parts[1]
+            })
 
-    module.exit_json(changed=True, result=images)
+    module.exit_json(changed=True, images=images)
 
 
 if __name__ == '__main__':
