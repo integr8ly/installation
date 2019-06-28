e961be8d3c42c5d1a5562530f3a3de59e0e5e095
Steven Tobin <stobin@redhat.com>
2019-06-28 14:09:53 +0100

    replace base64 -d with base64 --decode

--- scripts/get_admin_secret.sh
+++ scripts/get_admin_secret.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-oc get secret admin-user-credentials --template='{{index .data "password"}}' -n sso | base64 -d
+oc get secret admin-user-credentials --template='{{index .data "password"}}' -n sso | base64 --decode
